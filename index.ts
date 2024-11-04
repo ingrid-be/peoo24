1991aac7cff8fb016cb7e500b73faea63ebdfebb
ingrid-be <ingrid.beatriz@escolar.ifrn.edu.br>
2024-11-04 16:15:43 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,14 @@
+//Declaração de variaveis com tipagem 
+//let nome: tipo = valor
+let nome: string = "bem vindo ao typescript";
+let numero: 452;
+let logico: boolean = true;
+let vetor : number[];
+let vetor2: number[] = [1,2,3,4];
+let vetor3 = [];
+
+function somar(valorA: number, valorB:number) :number {
+    return valorA + valorB;
+}
+
+console.log(nome);
